08557e4e26921daf2de4d674f88f492cc2f57a1e
unknown <gianfranco@focoazul.com>
2023-07-12 10:44:46 -0300

    SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,31 @@ import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero";
 import NavMenu from "@/components/NavMenu";
 import { Providers } from "./providers";
+import Head from 'next/head';
+import ImageSeo from '@/components/NavMenu/Brand.svg'
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-
-
   return (
     <Providers>
       <div className="hero-back">
+        <Head>
+          <title>Advantech datos SEO</title>
+          <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+          <meta name="google" content="notranslate" key="notranslate" />
+          <meta name="description" content="Aquí estamos poniendo una breve description" />
+          <meta property="og:title" content="Advantech datos SEO" />
+          <meta property="og:description" content="Aquí estamos poniendo una breve description" />
+          <meta property="og:image" content={ImageSeo} />
+        </Head>
         <>
           <header className="w-full relative">
             <NavMenu />
           </header>
-          <Hero/>
-          <SeccionProductos/>
-          <SeccionRecursos/>
+          <Hero />
+          <SeccionProductos />
+          <SeccionRecursos />
           <SeccionAyuda />
           <SeccionCreaTuCuenta />
           <Footer />
