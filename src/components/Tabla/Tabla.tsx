8e83a82e61736aa92468a6a4bd5c02f3b2579b82
unknown <gianfranco@focoazul.com>
2023-08-14 10:52:18 -0300

    cambio de buscador

--- src/components/Tabla/Tabla.tsx
+++ src/components/Tabla/Tabla.tsx
@@ -1,37 +1,60 @@
+"use client";
+import React, { useState, useEffect } from 'react';
 import './Tabla.css';
 
 function Tabla() {
-  return (
-    <div className="tabla-container">
-      <h3 className="tabla-title-h3">Rangos de precio</h3>
-      <table className="tabla-card">
-        <thead className="tabla-head">
-          <tr>
-            <th className="tabla-title-i">Rango de créditos</th>
-            <th className="tabla-title-d">Costo por crédito</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="tabla-text">0 - 4999</td>
-            <td className="tabla-text">$0.11</td>
-          </tr>
-          <tr className="tabla-tr">
-            <td className="tabla-text">5000 - 9999</td>
-            <td className="tabla-text">$0.10</td>
-          </tr>
-          <tr>
-            <td className="tabla-text">10000 - 14999</td>
-            <td className="tabla-text">$0.09</td>
-          </tr>
-          <tr className="tabla-tr">
-            <td className="tabla-text">15000 - 19999</td>
-            <td className="tabla-text">$0.08</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    const [priceTiers, setPriceTiers] = useState<any[]>([]);
+
+    useEffect(() => {
+        // Lógica para obtener los datos de la API y establecer los price tiers
+        planalacarta().then((data) => {
+            setPriceTiers(data);
+        });
+    }, []);
+
+    return (
+        <div className="tabla-container">
+            <h3 className="tabla-title-h3">Rangos de precio</h3>
+            <table className="tabla-card">
+                <thead className="tabla-head">
+                    <tr>
+                        <th className="tabla-title-i">Rango de créditos</th>
+                        <th className="tabla-title-d">Costo por crédito</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {priceTiers.map((tier, index) => (
+                        <tr key={index} className={index % 2 === 0 ? '' : 'tabla-tr'}>
+                            <td className="tabla-text">{`${tier.attributes.minimo} - ${tier.attributes.maximo}`}</td>
+                            <td className="tabla-text">{`$${tier.attributes.preciocredito.toFixed(2)}`}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+async function planalacarta() {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/plan-a-la-cartas`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_KEY}`,
+            },
+            cache: 'no-store',
+        });
+
+        if (response.status !== 200) {
+            throw new Error(`Failed to fetch data, ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        throw new Error(`Failed to fetch data, ${error}`);
+    }
 }
 
 export default Tabla;
