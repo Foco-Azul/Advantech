898e0783567ccde5779a905db4efa5b0993e6508
po12133 <vargascarlos12123@gmail.com>
2023-09-28 06:09:07 -0400

    cambio en las imagenes del sitio

--- src/components/Carrusel/imageByIndex.ts
+++ src/components/Carrusel/imageByIndex.ts
@@ -1,9 +1,12 @@
-import image1 from './images/Screenshot 1.png';
-import image2 from './images/Screenshot 2.png';
-import image3 from './images/Screenshot 1.png';
-import image4 from './images/Screenshot 2.png';
+import image1 from './images/Historial de busquedas  1.png';
+import image2 from './images/Historial de compras.png';
+import image3 from './images/Imagen de ingreso al sitio.png';
+import image4 from './images/Mi cuenta.png';
+import image5 from './images/Página de pagos.png';
+import image6 from './images/Pago exitoso.png';
+import image7 from './images/Resultados de busqueda.png';
 
-export const images: string[] = [image1.src, image2.src, image3.src, image4.src];
+export const images: string[] = [image1.src, image2.src, image3.src, image4.src, image5.src, image6.src, image7.src];
 
 const imageByIndex = (index: number): string => images[index % images.length];
 
