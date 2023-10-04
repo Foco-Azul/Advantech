60e3d163c2d11908c731c90aac0a90e6a950e2fd
po12133 <vargascarlos12123@gmail.com>
2023-10-04 13:58:25 -0400

    guia para busqueda por lote

--- src/components/GuiaDeUso/SeccionBusquedas/SeccionBusquedas.tsx
+++ src/components/GuiaDeUso/SeccionBusquedas/SeccionBusquedas.tsx
@@ -28,21 +28,21 @@ function SeccionBusquedas() {
               <h2>Obteniendo datos de una persona</h2>
               <p>
                 <span className="guia-subtitulo">Realizando una busqueda: </span>
-                En la pestaña de Búsqueda Simple, puedes realizar búsquedas sin necesidad de estar logueado. Puedes buscar por <strong>nombre</strong> (preferiblemente nombre completo) o por <strong>RUC</strong> (identificación de la persona).
+                En la pestaña "Búsqueda Simple", puedes llevar a cabo búsquedas sin necesidad de iniciar sesión. Tienes la opción de buscar por <strong>nombre</strong> (preferiblemente el nombre completo) o por el número de <strong>RUC</strong> (identificación de la persona).
                 <br />
-                Ademas puedes elegir las fuentes de los resultados que desees obtener, entre ellas estan:
+                Además, tendrás la opción de seleccionar las fuentes de resultados que deseas obtener, las cuales incluyen:
                 <ul>
                   <li><FontAwesomeIcon icon={faCircleCheck} size="xl" /><span>Denuncias o noticias del delito personales</span></li>
                   <li><FontAwesomeIcon icon={faCircleCheck} size="xl" /><span>Procesos Judiciales electrónicos personales</span></li>
                 </ul>
               </p>
               <p>
-                <span className="guia-subtitulo">Resultados de mi busqueda: </span>
+                <span className="guia-subtitulo">Resultados de tu Búsqueda: </span>
                 Los resultados de la búsqueda por nombre pueden coincidir con más de una persona, mientras que la búsqueda por RUC te dará resultados precisos de una sola persona.
               </p>
               <p>
                 <span className="guia-subtitulo">Filtrando los resultados: </span>
-                Independientemente de si realizas la búsqueda por <strong>nombre</strong> o <strong>RUC</strong>, podras filtrar los resultados por <strong>lugar</strong> o <strong>año</strong>. Además, podras realizar búsquedas específicas dentro de los resultados obtenidos 
+                Ya sea que realices la búsqueda por nombre o RUC, podrás aplicar filtros a los resultados por <strong>lugar</strong> o <strong>año</strong>. Además, tendrás la capacidad de llevar a cabo búsquedas específicas dentro de los resultados obtenidos.
               </p>
               <p>
                 <span className="guia-subtitulo">Obteniendo los resultados: </span>
@@ -55,20 +55,26 @@ function SeccionBusquedas() {
           </div>
           <div className={'uso_de_datos-pestaña-contenido ' + (activeTab === 'RECURSOS HUMANOS' ? 'contenido-activo' : '')}>
             <div className="seccion-titulo">
-              <h4>Tutorial de Búsqueda por Lote</h4>
-              <h2>Evaluación de personal</h2>
+              <h4>Guia para realizar una busqueda por lote</h4>
+              <h2>Obteniendo datos de multiples personas</h2>
               <p>
-                En la pestaña de Búsqueda por Lote, puedes realizar búsquedas más extensas relacionadas con la gestión de talento y estrategias de recursos humanos.
+                <span className="guia-subtitulo">Realizando una busqueda: </span>
+                En la pestaña "Búsqueda por Lote", primero, debes iniciar sesión y contar con un plan para poder realizar una búsqueda. En este caso, deberás cargar un archivo Excel (XLSX) que contenga únicamente <strong>nombres</strong> o números de <strong>RUC</strong> de las personas que deseas buscar.
+                <br />
+                Además, tendrás la opción de seleccionar las fuentes de resultados que deseas obtener, las cuales incluyen:
+                <ul>
+                  <li><FontAwesomeIcon icon={faCircleCheck} size="xl" /><span>Denuncias o noticias del delito personales</span></li>
+                  <li><FontAwesomeIcon icon={faCircleCheck} size="xl" /><span>Procesos Judiciales electrónicos personales</span></li>
+                </ul>
               </p>
-              <p>Aquí tienes algunos pasos para realizar una búsqueda por lote:</p>
-              <ol>
-                <li>Selecciona la pestaña "Búsqueda por Lote".</li>
-                <li>Inicia sesión en tu cuenta (si es necesario).</li>
-                <li>Sube un archivo CSV con los datos de las personas que deseas buscar.</li>
-                <li>Especifica los criterios de búsqueda si es necesario.</li>
-                <li>Procesa la búsqueda por lote.</li>
-                <li>Obtén y revisa los resultados para tomar decisiones informadas en la gestión de talento.</li>
-              </ol>
+              <p>
+                <span className="guia-subtitulo">Resultados de tu Búsqueda: </span>
+                Los resultados de la búsqueda por nombre pueden coincidir con más de una persona, mientras que la búsqueda por RUC te dará resultados precisos de una sola persona.
+              </p>
+              <p>
+                <span className="guia-subtitulo">Obteniendo los resultados: </span>
+                Para acceder a los resultados de la búsqueda, es necesario que inicies sesión, te suscribas a un plan y contar con los créditos requeridos para descargar los datos. Dependiendo de tu plan actual, tendrás la opción de obtener los resultados en formatos como <strong>PDF</strong>, <strong>XLSX</strong>, <strong>CSV</strong> o <strong>JSON</strong>.
+              </p>
             </div>
             <div className='imagen'>
               <video src="https://admin.advantech.com.ec/uploads/video.mp4" controls></video>
